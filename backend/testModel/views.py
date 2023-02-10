954f8019754bc55a61aee06265b19ad76ceb4961
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-02-10 16:01:16 +0300

    face_recognition implemented

--- backend/testModel/views.py
+++ backend/testModel/views.py
@@ -9,6 +9,7 @@ import tempfile
 import pymongo
 
 from .serializers import ImageSerializer
+from train.models import FaceName
 
 #connecting to mongodb
 client = pymongo.MongoClient("mongodb+srv://Kennedy:Les5OoybneIII08V@cluster0.dtj0s4t.mongodb.net/?retryWrites=true&w=majority")
@@ -20,7 +21,7 @@ db = client['face_encoding_vectors']
 collection = db['face_encodings']
 
 
-# fetching the face encodings from mongoDB
+# fetching the known face encodings from mongoDB
 def fetch_face_encodings():
     face_encodings_doc = list(collection.find({}))
     face_ids, face_encodings = [face_doc["face_id"] for face_doc in face_encodings_doc], [face_doc["face_encoding"] for face_doc in face_encodings_doc]
@@ -29,12 +30,39 @@ def fetch_face_encodings():
     RecognizeFaceView.known_face_ids = face_ids
 
 
+# retrieving all details of the matched face from mysql db
+def retrieve_face_name(face_id):
+    face_name_obj = FaceName.objects.get(face_id=face_id)
+    face_name = face_name_obj.face_name
+
+    return face_name
+
 #comparing the face encoding with the known encoding vectors
 def compareFaceVectors(face_encodings, face_locations):
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        matches = face_recognition.compare_faces( face_encoding)
+        #matching...returns boolean list...default tolerance 0.6
+        matches = face_recognition.compare_faces(RecognizeFaceView.known_face_encodings, face_encoding)
+        face_id = ""
+        print(matches)
 
+        # calculating similarity between unknown image and known images encodings
+        #the smaller the face_distance the more similar the face...Euclidean distance
+        face_distances = face_recognition.face_distance(RecognizeFaceView.known_face_encodings, face_encoding)
+        # retrieving min value
+        best_match = np.argmin(face_distances)
+        print(face_distances)
+
+        if matches[best_match]:
+            #getting the face id for the match
+            face_id = RecognizeFaceView.known_face_ids[best_match]
+            face_name = retrieve_face_name(face_id)
+
+            return face_name
+        else:
+            return None
+        
  
+ # the view for recognizing unknown image
 class RecognizeFaceView(generics.CreateAPIView):
     serializer_class = ImageSerializer
     known_face_encodings = []
@@ -68,8 +96,11 @@ class RecognizeFaceView(generics.CreateAPIView):
             if len(self.known_face_encodings) == 0:
                 # fetches known encodings if not available
                 fetch_face_encodings()
+            
+            #finding a match
+            face_name = compareFaceVectors(face_encodings, face_locations)
  
-            return Response({"message": "Testing Api"})
+            return Response(face_name)
         else:
             return Response({"detail": "Ensure the uploaded image has a face and its clear."}, status=400)
 
