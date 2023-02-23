784891d8a54f6027b126abd1a670ecb2929f1ac2
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-02-23 09:18:59 +0300

    allowed acces through cors

--- backend/home/settings.py
+++ backend/home/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #third party packages
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
 
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -61,6 +63,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'home.urls'
+CORS_URLS_REGEX = r"^/api/.*"
+CORS_ALLOWED_ORIGINS = []
+
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = [
+        'http://localhost:4200',
+        'https://localhost:4200',
+    ]
 
 TEMPLATES = [
     {
